239c7a5b7292efbd7d0b9bdafe171c94302a5f79
DUGINISAISHARAN <duginisaisharan@gmail.com>
2025-06-12 12:31:56 +0530

    added elancode logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,11 @@ export const metadata = {
   title: "Master Machine Learning, DSA in C++, MERN Stack | ProgrammerHub",
   description: "Unlock your coding potential with ProgrammerHub's expert-led courses. Learn Machine Learning, DSA in C++, the MERN Stack, and more. Start your journey to programming mastery today!",
   openGraph: {
-    images: './ProgrammerHubLogo.jpeg'
+    images: './elancode_Main_Logo.png'
   },
 
   icons:{
-    icon:"./ProgrammerHubLogo.jpeg"
+    icon:"./elancode_Main_Logo.png"
   }
 };
 
